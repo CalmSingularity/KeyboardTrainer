216129bc673bd8190723fbd983cd323fa5fdbfed
Simon Klimov <simon-klimov@yandex.ru>
2018-03-28 13:58:59 +0300

    Implemented all required functionality

--- KeyboardTrainer/MainWindow.xaml.cs
+++ KeyboardTrainer/MainWindow.xaml.cs
@@ -44,7 +44,6 @@ namespace KeyboardTrainer
 			Border border = new Border
 			{
 				Margin = new Thickness(2.0),
-				//Padding = new Thickness(0, -4.0, 0, 0),
 				BorderBrush = new SolidColorBrush(Colors.Black),
 				BorderThickness = new Thickness(1.5),
 				Background = new SolidColorBrush(backgroundColor),
@@ -79,9 +78,31 @@ namespace KeyboardTrainer
 	}
 
 	/// <summary>
+	/// A key (button) that represents a control key (which is not a printable character).
+	/// </summary>
+	class ControlKey : KeyboardButton
+	{
+		public ControlKey(string value, int row, int column, int columnSpan)
+			: base(value, value, row, column, columnSpan, Colors.LightGray)
+		{
+			TextElement.FontSize = 16.0;
+		}
+	}
+
+	/// <summary>
+	/// A key (button) that represents a printable character.
+	/// </summary>
+	class PrintableKey : KeyboardButton
+	{
+		public PrintableKey(string regularValue, string shiftValue, int row, int column, int columnSpan, Color backgroundColor)
+			: base(regularValue, shiftValue, row, column, columnSpan, backgroundColor)
+		{ }
+	}
+
+	/// <summary>
 	/// A key (button) that represents a single letter.
 	/// </summary>
-	class LetterKey : KeyboardButton
+	class LetterKey : PrintableKey
 	{
 		public LetterKey(string value, int row, int column, Color backgroundColor)
 			: base(value.ToLower(), value.ToUpper(), row, column, 2, backgroundColor)
@@ -101,9 +122,9 @@ namespace KeyboardTrainer
 	}
 
 	/// <summary>
-	/// A key (button) that represents a single special character (which is not a digit or a letter). 
+	/// A key (button) that represents a digit or a single special character. 
 	/// </summary>
-	class SpecialCharKey : KeyboardButton
+	class SpecialCharKey : PrintableKey
 	{
 		public SpecialCharKey(string regularValue, string shiftValue, int row, int column, int columnSpan, Color backgroundColor)
 			: base(regularValue, shiftValue, row, column, columnSpan, backgroundColor)
@@ -135,22 +156,10 @@ namespace KeyboardTrainer
 	/// <summary>
 	/// A key (button) that represents the Space key.
 	/// </summary>
-	class SpaceKey : SpecialCharKey
+	class SpaceKey : KeyboardButton
 	{
 		public SpaceKey(int row, int column, int columnSpan, Color backgroundColor)
 			: base("Space", "Space", row, column, columnSpan, backgroundColor)
-		{
-			TextElement.FontSize = 16.0;
-		}
-	}
-
-	/// <summary>
-	/// A key (button) that represents a control key (which is not a single printable character).
-	/// </summary>
-	class ControlKey : KeyboardButton
-	{
-		public ControlKey(string value, int row, int column, int columnSpan)
-			: base(value, value, row, column, columnSpan, Colors.LightGray)
 		{
 			TextElement.FontSize = 16.0;
 		}
@@ -162,85 +171,94 @@ namespace KeyboardTrainer
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		const int TEXT_LENGHT = 68;
+		const int N_PRINTABLE_CHARS = 47;
+		Random rand;
 		DateTime startTime;
 		int correctlyTypedTextLength;
 		int fails;
-		Dictionary<Key, KeyboardButton> keyboardButtons;  // to store buttons shown on the graphical keyboard in the main window
-		//Dictionary<char, char> printableCharacters;  // to store all characters available for text generation
+		Dictionary<Key, KeyboardButton> allKeyboardButtons;  // to store buttons shown on the graphical keyboard in the main window 
+		List<PrintableKey> allPrintableChars;  // to store all printable characters for the purposes of random text generation
 
 		public MainWindow()
 		{
 			InitializeComponent();
-			keyboardButtons = new Dictionary<Key, KeyboardButton>();
+			rand = new Random(DateTime.Now.Millisecond);
+			allKeyboardButtons = new Dictionary<Key, KeyboardButton>();
 
-			keyboardButtons[Key.Oem3] = new SpecialCharKey("`", "~", 0, 0, 2, Colors.Pink);
-			keyboardButtons[Key.D1] = new DigitKey("1", "!", 0, 2, Colors.Pink);
-			keyboardButtons[Key.D2] = new DigitKey("2", "@", 0, 4, Colors.Pink);
-			keyboardButtons[Key.D3] = new DigitKey("3", "#", 0, 6, Colors.Yellow);
-			keyboardButtons[Key.D4] = new DigitKey("4", "$", 0, 8, Colors.LawnGreen);
-			keyboardButtons[Key.D5] = new DigitKey("5", "%", 0, 10, Colors.DeepSkyBlue);
-			keyboardButtons[Key.D6] = new DigitKey("6", "^", 0, 12, Colors.DeepSkyBlue);
-			keyboardButtons[Key.D7] = new DigitKey("7", "&", 0, 14, Colors.MediumVioletRed);
-			keyboardButtons[Key.D8] = new DigitKey("8", "*", 0, 16, Colors.MediumVioletRed);
-			keyboardButtons[Key.D9] = new DigitKey("9", "(", 0, 18, Colors.Pink);
-			keyboardButtons[Key.D0] = new DigitKey("0", ")", 0, 20, Colors.Yellow);
-			keyboardButtons[Key.OemMinus] = new SpecialCharKey("-", "_", 0, 22, 2, Colors.LawnGreen);
-			keyboardButtons[Key.OemPlus] = new SpecialCharKey("=", "+", 0, 24, 2, Colors.LawnGreen);
-			keyboardButtons[Key.Back] = new ControlKey("Backspace", 0, 26, 4);
-			keyboardButtons[Key.Tab] = new ControlKey("Tab", 1, 0, 3);
-			keyboardButtons[Key.Q] = new LetterKey("Q", 1, 3, Colors.Pink);
-			keyboardButtons[Key.W] = new LetterKey("W", 1, 5, Colors.Yellow);
-			keyboardButtons[Key.E] = new LetterKey("E", 1, 7, Colors.LawnGreen);
-			keyboardButtons[Key.R] = new LetterKey("R", 1, 9, Colors.DeepSkyBlue);
-			keyboardButtons[Key.T] = new LetterKey("T", 1, 11, Colors.DeepSkyBlue);
-			keyboardButtons[Key.Y] = new LetterKey("Y", 1, 13, Colors.MediumVioletRed);
-			keyboardButtons[Key.U] = new LetterKey("U", 1, 15, Colors.MediumVioletRed);
-			keyboardButtons[Key.I] = new LetterKey("I", 1, 17, Colors.Pink);
-			keyboardButtons[Key.O] = new LetterKey("O", 1, 19, Colors.Yellow);
-			keyboardButtons[Key.P] = new LetterKey("p", 1, 21, Colors.LawnGreen);
-			keyboardButtons[Key.OemOpenBrackets] = new SpecialCharKey("[", "{", 1, 23, 2, Colors.LawnGreen);
-			keyboardButtons[Key.OemCloseBrackets] = new SpecialCharKey("]", "}", 1, 25, 2, Colors.LawnGreen);
-			keyboardButtons[Key.Oem5] = new SpecialCharKey("\\", "|", 1, 27, 3, Colors.LawnGreen);
-			keyboardButtons[Key.CapsLock] = new ControlKey("Caps Lock", 2, 0, 4);
-			keyboardButtons[Key.A] = new LetterKey("A", 2, 4, Colors.Pink);
-			keyboardButtons[Key.S] = new LetterKey("S", 2, 6, Colors.Yellow);
-			keyboardButtons[Key.D] = new LetterKey("D", 2, 8, Colors.LawnGreen);
-			keyboardButtons[Key.F] = new LetterKey("F", 2, 10, Colors.DeepSkyBlue);
-			keyboardButtons[Key.G] = new LetterKey("G", 2, 12, Colors.DeepSkyBlue);
-			keyboardButtons[Key.H] = new LetterKey("H", 2, 14, Colors.MediumVioletRed);
-			keyboardButtons[Key.J] = new LetterKey("J", 2, 16, Colors.MediumVioletRed);
-			keyboardButtons[Key.K] = new LetterKey("K", 2, 18, Colors.Pink);
-			keyboardButtons[Key.L] = new LetterKey("L", 2, 20, Colors.Yellow);
-			keyboardButtons[Key.OemSemicolon] = new SpecialCharKey(";", ":", 2, 22, 2, Colors.LawnGreen);
-			keyboardButtons[Key.OemQuotes] = new SpecialCharKey("'", "\"", 2, 24, 2, Colors.LawnGreen);
-			keyboardButtons[Key.Enter] = new ControlKey("Enter", 2, 26, 4);
-			keyboardButtons[Key.LeftShift] = new ControlKey("Shift", 3, 0, 5);
-			keyboardButtons[Key.Z] = new LetterKey("Z", 3, 5, Colors.Pink);
-			keyboardButtons[Key.X] = new LetterKey("X", 3, 7, Colors.Yellow);
-			keyboardButtons[Key.C] = new LetterKey("C", 3, 9, Colors.LawnGreen);
-			keyboardButtons[Key.V] = new LetterKey("V", 3, 11, Colors.DeepSkyBlue);
-			keyboardButtons[Key.B] = new LetterKey("B", 3, 13, Colors.DeepSkyBlue);
-			keyboardButtons[Key.N] = new LetterKey("N", 3, 15, Colors.MediumVioletRed);
-			keyboardButtons[Key.M] = new LetterKey("M", 3, 17, Colors.MediumVioletRed);
-			keyboardButtons[Key.OemComma] = new SpecialCharKey(",", "<", 3, 19, 2, Colors.Pink);
-			keyboardButtons[Key.OemPeriod] = new SpecialCharKey(".", ">", 3, 21, 2, Colors.Yellow);
-			keyboardButtons[Key.OemQuestion] = new SpecialCharKey("/", "?", 3, 23, 2, Colors.LawnGreen);
-			keyboardButtons[Key.RightShift] = new ControlKey("Shift", 3, 25, 5);
-			keyboardButtons[Key.LeftCtrl] = new ControlKey("Ctrl", 4, 0, 3);
-			keyboardButtons[Key.LWin] = new ControlKey("Win", 4, 3, 3);
-			keyboardButtons[Key.LeftAlt] = new ControlKey("Alt", 4, 6, 3);
-			keyboardButtons[Key.Space] = new SpaceKey(4, 9, 12, Colors.Orange);
-			keyboardButtons[Key.RightAlt] = new ControlKey("Alt", 4, 21, 3);
-			keyboardButtons[Key.RWin] = new ControlKey("Win", 4, 24, 3);
-			keyboardButtons[Key.RightCtrl] = new ControlKey("Ctrl", 4, 27, 3);
+			allKeyboardButtons[Key.Oem3] = new SpecialCharKey("`", "~", 0, 0, 2, Colors.Pink);
+			allKeyboardButtons[Key.D1] = new DigitKey("1", "!", 0, 2, Colors.Pink);
+			allKeyboardButtons[Key.D2] = new DigitKey("2", "@", 0, 4, Colors.Pink);
+			allKeyboardButtons[Key.D3] = new DigitKey("3", "#", 0, 6, Colors.Yellow);
+			allKeyboardButtons[Key.D4] = new DigitKey("4", "$", 0, 8, Colors.LawnGreen);
+			allKeyboardButtons[Key.D5] = new DigitKey("5", "%", 0, 10, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.D6] = new DigitKey("6", "^", 0, 12, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.D7] = new DigitKey("7", "&", 0, 14, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.D8] = new DigitKey("8", "*", 0, 16, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.D9] = new DigitKey("9", "(", 0, 18, Colors.Pink);
+			allKeyboardButtons[Key.D0] = new DigitKey("0", ")", 0, 20, Colors.Yellow);
+			allKeyboardButtons[Key.OemMinus] = new SpecialCharKey("-", "_", 0, 22, 2, Colors.LawnGreen);
+			allKeyboardButtons[Key.OemPlus] = new SpecialCharKey("=", "+", 0, 24, 2, Colors.LawnGreen);
+			allKeyboardButtons[Key.Back] = new ControlKey("Backspace", 0, 26, 4);
+			allKeyboardButtons[Key.Tab] = new ControlKey("Tab", 1, 0, 3);
+			allKeyboardButtons[Key.Q] = new LetterKey("Q", 1, 3, Colors.Pink);
+			allKeyboardButtons[Key.W] = new LetterKey("W", 1, 5, Colors.Yellow);
+			allKeyboardButtons[Key.E] = new LetterKey("E", 1, 7, Colors.LawnGreen);
+			allKeyboardButtons[Key.R] = new LetterKey("R", 1, 9, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.T] = new LetterKey("T", 1, 11, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.Y] = new LetterKey("Y", 1, 13, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.U] = new LetterKey("U", 1, 15, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.I] = new LetterKey("I", 1, 17, Colors.Pink);
+			allKeyboardButtons[Key.O] = new LetterKey("O", 1, 19, Colors.Yellow);
+			allKeyboardButtons[Key.P] = new LetterKey("p", 1, 21, Colors.LawnGreen);
+			allKeyboardButtons[Key.OemOpenBrackets] = new SpecialCharKey("[", "{", 1, 23, 2, Colors.LawnGreen);
+			allKeyboardButtons[Key.OemCloseBrackets] = new SpecialCharKey("]", "}", 1, 25, 2, Colors.LawnGreen);
+			allKeyboardButtons[Key.Oem5] = new SpecialCharKey("\\", "|", 1, 27, 3, Colors.LawnGreen);
+			allKeyboardButtons[Key.CapsLock] = new ControlKey("Caps Lock", 2, 0, 4);
+			allKeyboardButtons[Key.A] = new LetterKey("A", 2, 4, Colors.Pink);
+			allKeyboardButtons[Key.S] = new LetterKey("S", 2, 6, Colors.Yellow);
+			allKeyboardButtons[Key.D] = new LetterKey("D", 2, 8, Colors.LawnGreen);
+			allKeyboardButtons[Key.F] = new LetterKey("F", 2, 10, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.G] = new LetterKey("G", 2, 12, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.H] = new LetterKey("H", 2, 14, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.J] = new LetterKey("J", 2, 16, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.K] = new LetterKey("K", 2, 18, Colors.Pink);
+			allKeyboardButtons[Key.L] = new LetterKey("L", 2, 20, Colors.Yellow);
+			allKeyboardButtons[Key.OemSemicolon] = new SpecialCharKey(";", ":", 2, 22, 2, Colors.LawnGreen);
+			allKeyboardButtons[Key.OemQuotes] = new SpecialCharKey("'", "\"", 2, 24, 2, Colors.LawnGreen);
+			allKeyboardButtons[Key.Enter] = new ControlKey("Enter", 2, 26, 4);
+			allKeyboardButtons[Key.LeftShift] = new ControlKey("Shift", 3, 0, 5);
+			allKeyboardButtons[Key.Z] = new LetterKey("Z", 3, 5, Colors.Pink);
+			allKeyboardButtons[Key.X] = new LetterKey("X", 3, 7, Colors.Yellow);
+			allKeyboardButtons[Key.C] = new LetterKey("C", 3, 9, Colors.LawnGreen);
+			allKeyboardButtons[Key.V] = new LetterKey("V", 3, 11, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.B] = new LetterKey("B", 3, 13, Colors.DeepSkyBlue);
+			allKeyboardButtons[Key.N] = new LetterKey("N", 3, 15, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.M] = new LetterKey("M", 3, 17, Colors.MediumVioletRed);
+			allKeyboardButtons[Key.OemComma] = new SpecialCharKey(",", "<", 3, 19, 2, Colors.Pink);
+			allKeyboardButtons[Key.OemPeriod] = new SpecialCharKey(".", ">", 3, 21, 2, Colors.Yellow);
+			allKeyboardButtons[Key.OemQuestion] = new SpecialCharKey("/", "?", 3, 23, 2, Colors.LawnGreen);
+			allKeyboardButtons[Key.RightShift] = new ControlKey("Shift", 3, 25, 5);
+			allKeyboardButtons[Key.LeftCtrl] = new ControlKey("Ctrl", 4, 0, 3);
+			allKeyboardButtons[Key.LWin] = new ControlKey("Win", 4, 3, 3);
+			allKeyboardButtons[Key.LeftAlt] = new ControlKey("Alt", 4, 6, 3);
+			allKeyboardButtons[Key.Space] = new SpaceKey(4, 9, 12, Colors.Orange);
+			allKeyboardButtons[Key.RightAlt] = new ControlKey("Alt", 4, 21, 3);
+			allKeyboardButtons[Key.RWin] = new ControlKey("Win", 4, 24, 3);
+			allKeyboardButtons[Key.RightCtrl] = new ControlKey("Ctrl", 4, 27, 3);
 
-			foreach (KeyboardButton keyboardButton in keyboardButtons.Values)
-			{
+			// implementing graphical keyboard in the main window
+			foreach (KeyboardButton keyboardButton in allKeyboardButtons.Values)  
 				keyboardGrid.Children.Add(keyboardButton.GridElement);
-				//if (keyboardButton is )
-			}
+
+			// populating printable characters list for the for the purposes of random text generation
+			allPrintableChars = allKeyboardButtons.Values.OfType<PrintableKey>().ToList<PrintableKey>();  
 		}
 
+		private void sliderDifficulty_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			tbDifficulty.Text = Math.Round(sliderDifficulty.Value).ToString();
+		}
 
 		private void btnStart_Click(object sender, RoutedEventArgs e)
 		{
@@ -258,20 +276,19 @@ namespace KeyboardTrainer
 			{
 				difficulty = int.Parse(tbDifficulty.Text);
 
-				if (difficulty > 47)
-					difficulty = 47;
-				else if (difficulty < 5)
-					difficulty = 5;
+				if (difficulty > N_PRINTABLE_CHARS)
+					difficulty = N_PRINTABLE_CHARS;
+				else if (difficulty < 2)
+					difficulty = 2;
 			}
 			catch (FormatException fe)
 			{
-				difficulty = 10;
-				tbDifficulty.Text = "10";
+				difficulty = 12;
+				tbDifficulty.Text = "12";
 			}
 			tbDifficulty.Text = difficulty.ToString();
 			tbGeneratedText.Text = GenerateText(difficulty);
 			tbTypedText.Focus();
-			//MessageBox.Show(tbGeneratedText.Text.Length.ToString());
 		}
 
 		private void btnStop_Click(object sender, RoutedEventArgs e)
@@ -289,10 +306,10 @@ namespace KeyboardTrainer
 
 		private void mainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
 		{
-			if (!keyboardButtons.ContainsKey(e.Key))
+			if (!allKeyboardButtons.ContainsKey(e.Key))
 				return;
 
-			keyboardButtons[e.Key].GridElement.Effect = new DropShadowEffect();
+			allKeyboardButtons[e.Key].GridElement.Effect = new DropShadowEffect();
 
 			if (e.Key == Key.LeftShift || e.Key == Key.RightShift || e.Key == Key.CapsLock)
 			{
@@ -310,8 +327,11 @@ namespace KeyboardTrainer
 				if (tbTypedText.Text.Length > 0)
 				{
 					tbTypedText.Text = tbTypedText.Text.Remove(tbTypedText.Text.Length - 1);
-					if (correctlyTypedTextLength > tbTypedText.Text.Length)
+					if (correctlyTypedTextLength >= tbTypedText.Text.Length)
+					{
 						correctlyTypedTextLength = tbTypedText.Text.Length;
+						tbTypedText.Foreground = new SolidColorBrush(Colors.Black);
+					}
 					tbTypedText.Select(0, correctlyTypedTextLength);
 				}
 				return;
@@ -319,15 +339,20 @@ namespace KeyboardTrainer
 
 			if (e.Key == Key.Space)
 				tbTypedText.AppendText(" ");
-			else if (keyboardButtons[e.Key] is LetterKey || keyboardButtons[e.Key] is SpecialCharKey)
-				tbTypedText.AppendText(keyboardButtons[e.Key].TextElement.Text);
+			else if (allKeyboardButtons[e.Key] is LetterKey || allKeyboardButtons[e.Key] is SpecialCharKey)
+				tbTypedText.AppendText(allKeyboardButtons[e.Key].TextElement.Text);
 
-			//if (correctlyTypedTextLength < tbTypedText.Text.Length && correctlyTypedTextLength < tbGeneratedText.Text.Length)
-			//{
 			if (tbGeneratedText.Text[correctlyTypedTextLength] == tbTypedText.Text[correctlyTypedTextLength])
 				correctlyTypedTextLength++;
-			if (tbGeneratedText.Text[tbTypedText.Text.Length - 1] != tbTypedText.Text[tbTypedText.Text.Length - 1])
-				fails++;
+
+			if (tbTypedText.Text.Length <= tbGeneratedText.Text.Length)
+				if (tbGeneratedText.Text[tbTypedText.Text.Length - 1] != tbTypedText.Text[tbTypedText.Text.Length - 1])
+					fails++;
+
+			if (correctlyTypedTextLength == tbTypedText.Text.Length)
+				tbTypedText.Foreground = new SolidColorBrush(Colors.Black);
+			else
+				tbTypedText.Foreground = new SolidColorBrush(Colors.Red);
 
 			tbFails.Text = fails.ToString();
 			tbSpeed.Text = Math.Round(correctlyTypedTextLength / (DateTime.Now - startTime).TotalMinutes).ToString();
@@ -336,17 +361,17 @@ namespace KeyboardTrainer
 			if (correctlyTypedTextLength == tbGeneratedText.Text.Length)
 			{
 				Stop();
-				MessageBox.Show("Congrats! You successfully typed the whole text", "Finished", MessageBoxButton.OK);
+				MessageBox.Show("Congrats! You have successfully typed the whole text", "Finished", MessageBoxButton.OK);
 				RefreshKeyboard();
 			}
 		}
 
 		private void mainWindow_PreviewKeyUp(object sender, KeyEventArgs e)
 		{
-			if (!keyboardButtons.ContainsKey(e.Key))
+			if (!allKeyboardButtons.ContainsKey(e.Key))
 				return;
 
-			keyboardButtons[e.Key].GridElement.Effect = null;
+			allKeyboardButtons[e.Key].GridElement.Effect = null;
 
 			if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
 				RefreshKeyboard();
@@ -354,22 +379,11 @@ namespace KeyboardTrainer
 			e.Handled = true;
 		}
 
-
-		private void mainWindow_KeyDown(object sender, KeyEventArgs e)
-		{
-
-		}
-
-		private void mainWindow_KeyUp(object sender, KeyEventArgs e)
-		{
-
-		}
-
 		private void RefreshKeyboard()
 		{
 			bool shiftIsOn = Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
 			bool capsIsOn = Keyboard.IsKeyToggled(Key.CapsLock);
-			foreach (KeyboardButton keyboardButton in keyboardButtons.Values)
+			foreach (KeyboardButton keyboardButton in allKeyboardButtons.Values)
 			{
 				keyboardButton.RefreshText(shiftIsOn, capsIsOn);
 				keyboardButton.GridElement.Effect = null;
@@ -378,8 +392,30 @@ namespace KeyboardTrainer
 
 		private string GenerateText(int difficulty)
 		{
-			// length = 68
-			return "Hello, World! Hello, World!";
+			List<char> randomChars = new List<char>();  // a list of randomly chosen chars according to the difficulty level to generate a text string from
+			int randomIndex;
+
+			for (int i = 0; i < difficulty; ++i)  // add random characters to the list
+			{
+				randomIndex = rand.Next(N_PRINTABLE_CHARS);
+				randomChars.Add(allPrintableChars.ElementAt(randomIndex).Value[0]);
+				if (cbCaseSensitive.IsChecked == true)
+					randomChars.Add(allPrintableChars.ElementAt(randomIndex).ShiftValue[0]);
+			}
+
+			for (int i = 5; i <= difficulty; i += 5)  // add some number of spaces to the list relative to the length of the list
+			{
+				randomChars.Add(' ');
+				if (cbCaseSensitive.IsChecked == true)
+					randomChars.Add(' ');
+			}
+
+			string result = "";
+			for (int i = 0; i < TEXT_LENGHT; ++i)
+			{
+				result += randomChars[rand.Next(randomChars.Count)];
+			}
+			return result;
 		}
 
 	}
